4ce569c507d5ca4a1e87715f6a904d491a500f57
abhipunj01 <abhishek.punj@dentsu.com>
2023-03-18 11:16:39 +0530

    Mocking Network Calls

--- src/test/java/abhiLearns/NetworkLogActivity.java
+++ src/test/java/abhiLearns/NetworkLogActivity.java
@@ -1,12 +1,12 @@
 package abhiLearns;
 
-import org.asynchttpclient.Response;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v110.network.Network;
 import org.openqa.selenium.devtools.v110.network.model.Request;
+import org.openqa.selenium.devtools.v110.network.model.Response;
 
 import java.sql.PreparedStatement;
 import java.util.Optional;
@@ -19,19 +19,19 @@ public class NetworkLogActivity {
 
         DevTools devTools = driver.getDevTools();
         devTools.createSession();
-        devTools.send(Network.enable(Optional.empty(),Optional.empty(),Optional.empty()));
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
 
-        devTools.addListener(Network.requestWillBeSent(),request ->{
+        devTools.addListener(Network.requestWillBeSent(), request -> {
 
             Request req = request.getRequest();
-            System.out.println("Requested URL: "+req.getUrl());
+            System.out.println("Requested URL: " + req.getUrl());
         });
 
         //Event will get fired, for that we have listeners
         devTools.addListener(Network.responseReceived(), response -> {
-            Response res = (Response) response.getResponse();
-            System.out.println("Response URL"+res.getUri());
-            System.out.println("Status Code"+res.getStatusCode());
+            Response res = response.getResponse();
+            System.out.println("Status Code: " + res.getStatus());
+            System.out.println("Headers: " + res.getHeaders().toString());
         });
 
         driver.get("https://rahulshettyacademy.com/angularAppdemo/");
